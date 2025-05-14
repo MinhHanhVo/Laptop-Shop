288728070f49c7e238bbe97a3b3c76d76584a937
hanhneee <hanhminhvo1809@gmail.com>
2025-05-14 23:33:35 +0700

    MVC-Express

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,23 @@
 const { log } = require("console");
 import express from "express";
 import 'dotenv/config'
+import webRoutes from "./routes/web";
 
 const app = express();
 const port = process.env.port || 8080;
 
 // config view engine
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', __dirname + '/views');
 
-app.get("/", (req, res) => {
-    res.render("home.ejs")
-})
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// config static file: images/css/js
+app.use(express.static('public'))
+// config routes
+webRoutes(app);
 
 app.listen(port, () => {
     console.log(`My app is running on port: ${port}`);
